220e80b858633c725c4e0e5977aab7e97775c6d1
sangjun <richellin7@gmail.com>
2016-04-20 11:51:19 +0900

    first commit

---
+++ tests/Sample2Test.php
@@ -0,0 +1,11 @@
+<?php
+Class Sample2Test extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function This_is_sample2_test()
+    {
+        $this->assertTrue(TRUE);
+    }
+}
---
+++ tests/SampleTest.php
@@ -0,0 +1,11 @@
+<?php
+Class SampleTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function This_is_sample_test()
+    {
+        $this->assertTrue(TRUE);
+    }
+}
